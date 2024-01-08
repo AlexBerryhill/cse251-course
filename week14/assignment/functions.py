29a462ae5a9a969136c9b8dcf71d735417c58f8e
Alexander Berryhill <amsb99@gmail.com>
2024-01-08 11:30:59 -0700

    Update functions.py

--- week14/assignment/functions.py
+++ week14/assignment/functions.py
@@ -1,7 +1,7 @@
 """
 Course: CSE 251, week 14
 File: functions.py
-Author: <your name>
+Author: Alex Berryhill
 
 Instructions:
 
@@ -43,40 +43,28 @@ You will lose 10% if you don't detail your part 1 and part 2 code below
 
 Describe how to speed up part 1
 
-To speed up Part 1, you can send requests simultaneously, 
-avoid redundant requests by remembering obtained information, 
-fetch multiple people's data together, optimize data storage, 
-and identify and optimize slow parts. This approach ensures 
-efficient use of time and resources, similar to ordering 
-multiple items at once, remembering previous choices, and 
-using smart organization methods. By implementing these 
-strategies, you can streamline the process, minimize delays, 
-and enhance overall performance.
+ Instead of waiting for each request to finish before moving on, 
+ I've started multiple requests at once using threads. This lets 
+ them run together, which can speed things up. I first get the 
+ family data in a separate thread, then create a family object and 
+ add it to the tree.
 
 Describe how to speed up part 2
 
-To speed up Part 2, you can do two things: use multiple threads 
-or processes to fetch family and person information at the same 
-time, and keep track of the data you've already fetched. By 
-using threads or processes, you can get information from the 
-server more quickly by making several requests simultaneously. 
-And by remembering the data you've already fetched, you won't 
-waste time requesting it again. These techniques will make 
-Part 2 faster and more efficient.
+I've made the breadth_fs_pedigree function faster by using a queue 
+and threading. It works like this: I put the initial family ID in 
+the queue, then use threads to request and process family, husband, 
+wife, and child data concurrently, making the whole process quicker 
+and more efficient.
 
 
 Extra (Optional) 10% Bonus to speed up part 3
 
-To speed up Part 3, you can cache previously fetched family and 
-person data so you don't have to request it again. This saves 
-time by avoiding redundant server calls. You can also use smart 
-data structures and algorithms to quickly access the information 
-you need instead of searching through everything. Another trick 
-is to fetch and process data at the same time using parallel 
-processing, like multi-threading or multi-processing. This way, 
-you can get things done faster by doing multiple tasks 
-simultaneously. These optimizations make Part 3 faster and more 
-efficient.
+I make things faster by using a thread pool executor. I put the 
+initial family ID in a queue and process family, husband, wife, and 
+child data concurrently within the thread limit. This way, you can 
+handle multiple requests at once, speeding up the process and getting 
+things done more efficiently.
 
 """
 from common import *
@@ -92,74 +80,74 @@ family_cache = {}
 person_cache_lock = threading.Lock()
 family_cache_lock = threading.Lock()
 
-def retrieve_family(family_id):
-    # Depricated
-    # Retrieve family information from the server
-    request = Request_thread(f'{TOP_API_URL}/family/{family_id}')
-    request.start()
-    request.join()
-    family_data = request.get_response()
-    return family_data
+# def retrieve_family(family_id):
+#     # Depricated
+#     # Retrieve family information from the server
+#     request = Request_thread(f'{TOP_API_URL}/family/{family_id}')
+#     request.start()
+#     request.join()
+#     family_data = request.get_response()
+#     return family_data
 
-def retrieve_person(person_id):
-    # Depricated
-    # Retrieve person information from the server
-    request = Request_thread(f'{TOP_API_URL}/person/{person_id}')
-    request.start()
-    request.join()
-    person_data = request.get_response()
-    return person_data
+# def retrieve_person(person_id):
+#     # Depricated
+#     # Retrieve person information from the server
+#     request = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+#     request.start()
+#     request.join()
+#     person_data = request.get_response()
+#     return person_data
 
-def process_family(family_data, tree):
-    # Depricated
-    if family_data is None:
-        return
-    family_id = family_data['id']
-    husband_id = family_data['husband_id']
-    wife_id = family_data['wife_id']
-    children_ids = family_data['children']
+# def process_family(family_data, tree):
+#     # Depricated
+#     if family_data is None:
+#         return
+#     family_id = family_data['id']
+#     husband_id = family_data['husband_id']
+#     wife_id = family_data['wife_id']
+#     children_ids = family_data['children']
 
-    if tree.does_family_exist(family_id):
-        return
+#     if tree.does_family_exist(family_id):
+#         return
 
-    next_level = []
+#     next_level = []
 
-    if husband_id in person_cache:
-        husband_data = person_cache[husband_id]
-    else:
-        husband_data = retrieve_person(husband_id)
-        with person_cache_lock:
-            person_cache[husband_id] = husband_data
+#     if husband_id in person_cache:
+#         husband_data = person_cache[husband_id]
+#     else:
+#         husband_data = retrieve_person(husband_id)
+#         with person_cache_lock:
+#             person_cache[husband_id] = husband_data
 
-    husband = Person(husband_data)
-    next_level.append(husband)
-    tree.add_person(husband)
+#     husband = Person(husband_data)
+#     next_level.append(husband)
+#     tree.add_person(husband)
 
-    if wife_id in person_cache:
-        wife_data = person_cache[wife_id]
-    else:
-        wife_data = retrieve_person(wife_id)
-        with person_cache_lock:
-            person_cache[wife_id] = wife_data
+#     if wife_id in person_cache:
+#         wife_data = person_cache[wife_id]
+#     else:
+#         wife_data = retrieve_person(wife_id)
+#         with person_cache_lock:
+#             person_cache[wife_id] = wife_data
 
-    wife = Person(wife_data)
-    next_level.append(wife)
-    tree.add_person(wife)
+#     wife = Person(wife_data)
+#     next_level.append(wife)
+#     tree.add_person(wife)
 
-    for child_id in children_ids:
-        if child_id in person_cache:
-            child_data = person_cache[child_id]
-        else:
-            child_data = retrieve_person(child_id)
-            with person_cache_lock:
-                person_cache[child_id] = child_data
+#     for child_id in children_ids:
+#         if child_id in person_cache:
+#             child_data = person_cache[child_id]
+#         else:
+#             child_data = retrieve_person(child_id)
+#             with person_cache_lock:
+#                 person_cache[child_id] = child_data
 
-        child = Person(child_data)
-        tree.add_person(child)
+#         child = Person(child_data)
+#         tree.add_person(child)
 
-    family = Family(family_data)
-    tree.add_family(family)
-    return next_level
+#     family = Family(family_data)
+#     tree.add_family(family)
+#     return next_level
 
 def depth_fs_pedigree(family_id, tree):
     request = Request_thread(f'{TOP_API_URL}/family/{family_id}')
